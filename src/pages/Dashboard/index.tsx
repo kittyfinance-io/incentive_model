7b97a7af98dfd37893c81a1e709555263a8fcf0d
mcbunnylau <mcbunnylau2013@gmail.com>
2022-07-12 01:12:26 +0100

    feat: replaced and added new query buttons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -60,48 +60,56 @@ const Dashboard = () => {
     early_withdrawal_rate: 0.5,
   });
 
+  const IProt2Prot = (protocolParams: IProtocol) => {
+    return new ProtocolParams(
+      stateProtocolParams.protocol_tax, // protocol tax
+      stateProtocolParams.peg_tax_re_mint_rate, // peg tax remint rate
+      stateProtocolParams.protocol_tax_public_vs_dao_ratio, // protocol tax to public
+      stateProtocolParams.supply_expansion_rate, // supply expansion rate -> the lower this is, the faster the theoretical price of LionAvax rises & the lower the LP staking APR
+
+      0.11, //   stateProtocolParams.marketing_cut, // marketing cut
+      0.3125, //   stateProtocolParams.team_cut, // team cut
+
+      stateProtocolParams.early_withdrawal_tax, // early withdrawal tax
+      stateProtocolParams.p1_vs_p3_supply_expansion, // p1 vs p3 supply expansion
+      stateProtocolParams.p1_vs_l1_reward_ratio, // p1 vs l1 reward ratio
+      stateProtocolParams.p3_vs_l3_reward_ratio, // p3 vs l3 reward ratio,
+      stateProtocolParams.reemission_timeframe // reemission timeframe of burned peg contributions in days
+    );
+  };
+
+  const IEnv2Env = (stateEnvInputs: IEnvInputs, protocolParams: IProtocol) => {
+    return new EnvInputs(
+      stateEnvInputs.avax_price_in_dollar, // avax price
+      stateEnvInputs.lion_price_in_dollar, // lion avax price
+      110000 / (100 * protocolParams.protocol_tax_public_vs_dao_ratio), // lion share price
+      stateEnvInputs.buying_volume, // buying vol
+      stateEnvInputs.selling_volume, // selling vol
+      stateEnvInputs.prev_volume,
+
+      stateEnvInputs.p1_liq_in_dollar, // p1 liquidity
+      stateEnvInputs.p1_pol_ratio, // pol ratio p1
+      stateEnvInputs.p3_liq_in_dollar, // p3 liq on avax (assuming 80% of the 10% community shares are staked bc they get rewards too), x2 for other side of pool, /2 for 2 chains on launch
+
+      stateEnvInputs.lion_staking_pool_size_in_tokens, // lion staking tokens, assuming 5k in p1, 5k in p3
+
+      stateEnvInputs.p1_staked_in_dollar,
+      stateEnvInputs.p3_staked_in_dollar,
+      stateEnvInputs.p1_locked_in_dollar,
+      stateEnvInputs.p3_locked_in_dollar,
+
+      stateEnvInputs.pending_liq_contrib_rewards, // pending liq contribution rewards
+      stateEnvInputs.timeframe, // timeframe in days
+      stateEnvInputs.early_withdrawal_rate // early withdrawal rate of P1 and P3 stakers
+    );
+  };
+
   useEffect(() => {
     __debounce(
       () => {
-        const protocolParams = new ProtocolParams(
-          stateProtocolParams.protocol_tax, // protocol tax
-          stateProtocolParams.peg_tax_re_mint_rate, // peg tax remint rate
-          stateProtocolParams.protocol_tax_public_vs_dao_ratio, // protocol tax to public
-          stateProtocolParams.supply_expansion_rate, // supply expansion rate -> the lower this is, the faster the theoretical price of LionAvax rises & the lower the LP staking APR
+        const protocolParams = IProt2Prot(stateProtocolParams);
 
-          0.11, //   stateProtocolParams.marketing_cut, // marketing cut
-          0.3125, //   stateProtocolParams.team_cut, // team cut
-
-          stateProtocolParams.early_withdrawal_tax, // early withdrawal tax
-          stateProtocolParams.p1_vs_p3_supply_expansion, // p1 vs p3 supply expansion
-          stateProtocolParams.p1_vs_l1_reward_ratio, // p1 vs l1 reward ratio
-          stateProtocolParams.p3_vs_l3_reward_ratio, // p3 vs l3 reward ratio,
-          stateProtocolParams.reemission_timeframe // reemission timeframe of burned peg contributions in days
-        );
-
-        const envInputs = new EnvInputs(
-          stateEnvInputs.avax_price_in_dollar, // avax price
-          stateEnvInputs.lion_price_in_dollar, // lion avax price
-          110000 / (100 * protocolParams.protocol_tax_public_vs_dao_ratio), // lion share price
-          stateEnvInputs.buying_volume, // buying vol
-          stateEnvInputs.selling_volume, // selling vol
-          stateEnvInputs.prev_volume,
-
-          stateEnvInputs.p1_liq_in_dollar, // p1 liquidity
-          stateEnvInputs.p1_pol_ratio, // pol ratio p1
-          stateEnvInputs.p3_liq_in_dollar, // p3 liq on avax (assuming 80% of the 10% community shares are staked bc they get rewards too), x2 for other side of pool, /2 for 2 chains on launch
-
-          stateEnvInputs.lion_staking_pool_size_in_tokens, // lion staking tokens, assuming 5k in p1, 5k in p3
-
-          stateEnvInputs.p1_staked_in_dollar,
-          stateEnvInputs.p3_staked_in_dollar,
-          stateEnvInputs.p1_locked_in_dollar,
-          stateEnvInputs.p3_locked_in_dollar,
-
-          stateEnvInputs.pending_liq_contrib_rewards, // pending liq contribution rewards
-          stateEnvInputs.timeframe, // timeframe in days
-          stateEnvInputs.early_withdrawal_rate // early withdrawal rate of P1 and P3 stakers
-        );
+        const envInputs = IEnv2Env(stateEnvInputs, stateProtocolParams);
 
         let _chart: IChart[] = [];
 
@@ -206,7 +214,20 @@ const Dashboard = () => {
   }, [searchParams]);
   console.log("stateEnvInputs", stateEnvInputs);
 
-  const encodeQuery = () => {
+  const encodeQuery = (mode: string) => {
+    // get_roi
+    let newRoi = get_roi(
+      1000,
+      IEnv2Env(stateEnvInputs, stateProtocolParams),
+      IProt2Prot(stateProtocolParams),
+      mode,
+      false
+    );
+
+    console.log(newRoi?.resulting_env);
+
+    let resulting_env: IEnvInputs | undefined = newRoi?.resulting_env;
+
     let query: string = window.location.href.split("?")[0] + "?";
     Object.keys(stateProtocolParams).forEach((item: string, index: number) => {
       //   console.log("item", item);
@@ -218,15 +239,21 @@ const Dashboard = () => {
       query += `${item}=${stateProtocolParams[item as keyof IProtocol]}`;
       console.log("query :>> ", query);
     });
-    Object.keys(stateEnvInputs).forEach((item: string, index: number) => {
-      //   console.log("item", item);
-      //   console.log(
-      //     "stateProtocolParams[item as keyof IProtocol]",
-      //     stateProtocolParams[item as keyof IProtocol]
-      //   );
+    Object.keys(resulting_env ? resulting_env : stateEnvInputs).forEach(
+      (item: string, index: number) => {
+        //   console.log("item", item);
+        //   console.log(
+        //     "stateProtocolParams[item as keyof IProtocol]",
+        //     stateProtocolParams[item as keyof IProtocol]
+        //   );
 
-      query += `&${item}=${stateEnvInputs[item as keyof IEnvInputs]}`;
-    });
+        query += `&${item}=${
+          resulting_env
+            ? resulting_env[item as keyof IEnvInputs]
+            : stateEnvInputs[item as keyof IEnvInputs]
+        }`;
+      }
+    );
     query += `&investAmount=${investAmount}`;
     console.log("query", query);
     window.open(query, "_blank");
@@ -243,16 +270,39 @@ const Dashboard = () => {
         }}
       >
         <Paper sx={{ px: 5 }}>
-          <Button
+          {/* <Button
             variant="contained"
             sx={{ ml: 3, mt: 3 }}
             onClick={() => encodeQuery()}
           >
             Encode Query
-          </Button>
+          </Button> */}
           <ChartROI chartData={chartData} />
         </Paper>
       </Box>
+
+      <Grid container spacing={2}>
+        <Grid item>
+          <Button onClick={() => encodeQuery("stake lion")}>stake lion</Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={() => encodeQuery("stake shares")}>
+            stake shares
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={() => encodeQuery("stake p1")}>stake p1</Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={() => encodeQuery("stake p3")}>stake p3</Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={() => encodeQuery("lock p1")}>lock p1</Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={() => encodeQuery("lock p3")}>lock p3</Button>
+        </Grid>
+      </Grid>
 
       <Grid container columnSpacing={5} marginTop={5} sx={{ px: 5 }}>
         <Grid item xs={12} md={4}>
